5339e6b306c7a412b42636683a95afae01191b71
System Administrator <root@Dannyyusis-MacBook-Pro.local>
2022-10-22 17:27:58 +0200

    Bunch of header stuff

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div class="container-fluid p-0" :class=" light_mode ? 'background' : 'dark_background'">
       <div class="header-component">
         <div class="header">
-          <LoggedOutHeader @Mode="light_mode = !light_mode"/>
+          <LoggedOutHeader v-if="!logged" @Mode="light_mode = !light_mode"/>
+          <LoggedInHeader v-else @Mode="light_mode = !light_mode"/>
         </div>
       </div>
       <router-view></router-view>
@@ -13,11 +14,18 @@
 <script>
 import MainFooter from '@/components/MainFooter.vue'
 import LoggedOutHeader from '@/components/LoggedOutHeader.vue'
+import LoggedInHeader from './components/products_components/LoggedInHeader.vue';
 export default {
   name: 'App',
   components:{
     LoggedOutHeader,
-    MainFooter
+    MainFooter,
+    LoggedInHeader
+  },
+  data(){
+    return{
+      logged: true
+    }
   },
   mounted(){
     this.$router.push("/home")
