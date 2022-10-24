dccf54ccf1133b17072808d1248e63f5d91d1687
System Administrator <root@Dannyyusis-MacBook-Pro.local>
2022-10-24 20:15:07 +0200

    Login warnings

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-fluid login_body">
+  <div class="container-fluid login_body" @click="warning_login = false, warning_register = false">
     <div class="section">
       <div class="container">
         <div class="row full-height justify-content-center">
@@ -16,13 +16,40 @@
                 name="reg-log"
               />
               <label for="reg-log"></label>
+              <div v-if="warning_login" class="warning_base">
+                <div class="pt-4 warning_error">
+                  <div class="alert alert-warning" role="alert">
+                    Whoops! These credentials do not match!
+                  </div>
+                </div>
+              </div>
+              <div v-else-if="succes_login" class="succes_base">
+                <div class="pt-4 warning_succes">
+                  <div class="alert alert-succes" role="alert">
+                    Welcome back {{name}} !!
+                  </div>
+                </div>
+              </div>
+              <div v-else-if="succes_register" class="succes_base">
+                <div class="pt-4 warning_succes">
+                  <div class="alert alert-succes" role="alert">
+                    Congrats! You can now login into your account.
+                  </div>
+                </div>
+              </div>
+              <div v-else-if="warning_register" class="warning_base">
+                <div class="pt-4 warning_error">
+                  <div class="alert alert-warning" role="alert">
+                    Whoops! Please check if both your passwords match <br> or if it's at least 8 characters c:
+                  </div>
+                </div>
+              </div>
               <div class="card-3d-wrap mx-auto">
                 <div class="card-3d-wrapper">
                   <div class="card-front">
                     <div class="center-wrap">
                       <div class="section text-center">
                         <h4 class="mb-3 pb-3">Log In</h4>
-                        <p v-if="warning">Wrong credentials, <br>please try again ♥</p>
                         <div class="form-group">
                           <input
                             type="email"
@@ -129,7 +156,11 @@ export default {
             email: undefined,
             password: undefined,
             rep_password: undefined,
-            warning: false
+            warning_login: false,
+            warning_register: false,
+            succes_login: false,
+            succes_register: false,
+            name: undefined
         }
     },
     methods:{
@@ -140,13 +171,15 @@ export default {
                 'password':this.password
             }).then((response) =>{
                 if(response.data.data.token){
+                    this.name = response.data.data.user.name;
+                    this.succes_login = true;
                     localStorage.setItem('authToken',response.data.data.token);
                     this.$store.commit('Logged', true)
                     this.$router.push('/home');
                 }
             }).catch((error)=>{
                 console.log(error)
-                this.warning = true
+                this.warning_login = true
             })
         },
         Register(){
@@ -156,10 +189,14 @@ export default {
                 'password': this.password,
                 'password_confirmation': this.rep_password
             }).then((response) => {
-                console.log(response)
+              if(response.data.data.token){
+                    this.succes_register = true;
+                    localStorage.setItem('authToken',response.data.data.token);
+                    this.$store.commit('Logged', true)
+                }
             }).catch((error)=>{
                 console.log(error);
-                this.warning = true;
+                this.warning_register = true;
             })
         }
     }
